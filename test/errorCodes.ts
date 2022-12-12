dd4dc76a9209ddcefb2835c1426010931845a724
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-12-12 17:11:02 +0100

    remove test code

--- test/errorCodes.ts
+++ test/errorCodes.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 import packageVersion from "../package.json";
 import { Up2Pay } from "../src/up2pay.js";
 
-import payboxConfig from "../paybox-config.json";
+//import payboxConfig from "../paybox-config.json";
 import exp from "constants";
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
